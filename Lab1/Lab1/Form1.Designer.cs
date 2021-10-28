b4afed1bb60d337e676c5563c6f8b939468f8632
michalkornaus <56958103+michalkornaus@users.noreply.github.com>
2021-10-28 12:03:07 +0200

    duperele

--- Lab1/Lab1/Form1.Designer.cs
+++ Lab1/Lab1/Form1.Designer.cs
@@ -30,6 +30,8 @@
         {
             this.panel1 = new System.Windows.Forms.Panel();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // panel1
@@ -39,6 +41,7 @@
             this.panel1.Size = new System.Drawing.Size(776, 354);
             this.panel1.TabIndex = 0;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseClick);
             // 
             // button1
             // 
@@ -46,20 +49,40 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(116, 35);
             this.button1.TabIndex = 1;
-            this.button1.Text = "RYSUJ AX";
+            this.button1.Text = "RYSUJ";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(183, 386);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(93, 35);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "PRZEPISZ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(297, 386);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -67,6 +90,8 @@
 
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- Lab1/Lab1/Form1.cs
+++ Lab1/Lab1/Form1.cs
@@ -13,6 +13,9 @@ namespace Lab1
     public partial class Form1 : Form
     {
         public bool show = false;
+        public bool przepisz = false;
+        public int X, Y;
+        List <Point> pathPoints = new List<Point>();
         public Form1()
         {
             InitializeComponent();
@@ -20,38 +23,77 @@ namespace Lab1
 
         private void panel1_Paint(object sender, PaintEventArgs e)
         {
+            Rectangle rect1 = new Rectangle(new Point(500, 50), new Size(50, 50));
+            Rectangle rect2 = new Rectangle(new Point(550, 50), new Size(50, 50));
+            Rectangle rect3 = new Rectangle(new Point(525, 115), new Size(50, 100));
+            e.Graphics.DrawEllipse(Pens.CornflowerBlue, rect1);
+            e.Graphics.DrawPie(Pens.DarkGray, rect1, 50f, 180f);
+            e.Graphics.DrawEllipse(Pens.CornflowerBlue, rect2);
+            e.Graphics.DrawPie(Pens.DarkGray, rect2, -50f, 180f);
+            e.Graphics.DrawEllipse(Pens.Red, rect3);
             if (show == true)
             {
-                e.Graphics.DrawLine(Pens.Red, 10, 10, 200, 200);
+                /*e.Graphics.DrawLine(Pens.Red, 10, 10, 200, 200);
                 Rectangle rect = new Rectangle(new Point(50, 50), new Size(50, 50));
-                
-                e.Graphics.DrawEllipse(Pens.CornflowerBlue, rect);
-               
-            }
-            else
-            {
-                Rectangle rect1 = new Rectangle(new Point(500, 50), new Size(50, 50));
-                Rectangle rect2 = new Rectangle(new Point(550, 50), new Size(50, 50));
-                Rectangle rect3 = new Rectangle(new Point(525, 75), new Size(50, 100));
-                e.Graphics.DrawEllipse(Pens.CornflowerBlue, rect1);
-                e.Graphics.DrawPie(Pens.DarkGray, rect1, 50f, 180f);
-                e.Graphics.DrawEllipse(Pens.CornflowerBlue, rect2);
-                e.Graphics.DrawPie(Pens.DarkGray, rect2, 50f, 180f);
-                e.Graphics.DrawEllipse(Pens.Red, rect3);
+                e.Graphics.DrawEllipse(Pens.CornflowerBlue, rect);*/
                 Point[] points = new Point[3]
-                {
+                 {
                     new Point(500, 200),
                     new Point(550, 250),
                     new Point(600, 200),
+                 };
+                e.Graphics.DrawCurve(Pens.Green, points);
+                if (pathPoints.Count > 1)
+                {
+                    e.Graphics.DrawPolygon(Pens.DarkTurquoise, pathPoints.ToArray());
+                    e.Graphics.FillPolygon(Brushes.Red, pathPoints.ToArray());
+                }        
+            }
+            else
+            {
+                Point[] points = new Point[3]
+                {
+                    new Point(500, 300),
+                    new Point(550, 250),
+                    new Point(600, 300),
                 };
                 e.Graphics.DrawCurve(Pens.Green, points);
+                if (pathPoints.Count > 1)
+                    e.Graphics.DrawLines(Pens.DarkTurquoise, pathPoints.ToArray());
             }
+            int radius = 300;
+            e.Graphics.DrawEllipse(Pens.IndianRed, X - (radius / 2), Y - (radius / 2), radius, radius);
+            if(przepisz)
+            {
+                DrawStringPoint(e);
                 
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             show = !show;
+            panel1.Invalidate();
+        }
+        public void DrawStringPoint(PaintEventArgs e)
+        {
+            string drawString = textBox1.Text;
+            Font drawFont = new Font("Arial", 17);
+            SolidBrush drawBush = new SolidBrush(Color.Pink);
+            Point drawPoint = new Point(150, 150);
+            e.Graphics.DrawString(drawString, drawFont, drawBush, drawPoint);
+            przepisz = false;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            przepisz = true;
+            panel1.Invalidate();
+        }
+
+        private void panel1_MouseClick(object sender, MouseEventArgs e)
+        {
+            X = e.X; Y = e.Y;
+            pathPoints.Add(new Point(e.X, e.Y));
             panel1.Invalidate();
         }
     }
